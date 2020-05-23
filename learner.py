ed36f1a2342497ed5a05afd47e1261bccb136cbd
Eric Nutter <nttereg@gmail.com>
2020-05-23 14:47:04 -0500

    fixed bugs

--- learner.py
+++ learner.py
@@ -1,6 +1,5 @@
 import sys
 import json
-from collections import namedtuple
 from random import shuffle
 
 """
@@ -14,32 +13,40 @@ __email__ = "nuttereg@gmail.com"
 helpstr = """ lol """
 
 class Quiz:
-    def __init__(self, name='', cards = []):
-        self.name = name
-        self.cards = cards
-        self.size = 0
+    name = ''
+    cards = []
+    size = 0
+    def __init__(self, filelist = []):
+        if filelist == []:
+            # it may be a bad idea to allow this kind of call...
+            self.name = ''
+            self.cards = []
+            self.size = 0
+        else:
+            decks = []
+            for file in filelist:
+                names = filelist
+                names = [ names.pop(names.index(filename)) for filename in names ]
+                #print('names = ', names)
+                try:
+                    with open(file, 'r') as deckfile:
+                        decks.append(deckfile.readlines())
+                except IOError:
+                    print(file, "does not appear to exist")
+                    break
 
-    def from_file(self, filenames):
-        decks = []
-        for file in filenames:
-            try:
-                with open(file, 'r') as deckfile:
-                    decks.append(deckfile.readlines())
-            except IOError:
-                print(file, "does not appear to exist")
-                break
-
-        [[ self.cards.append(card) for card in deck ]  for deck in decks ]
+            [[ self.cards.append(card) for card in deck ]  for deck in decks ]
 
     def to_string(self):
+        """converts a json Quiz to the "legacy" format"""
         pass
 
     def to_json(self):
         """converts a String format deck to json"""
-        print(json.dumps({"name": self.name, "qa": [self.cards]}, \
+        json.dumps({"name": self.name, "qa": [self.cards]}, \
                           separators=(':', ','), \
                           sort_keys=True, \
-                          indent=4))
+                          indent=4)
     
     def study(self, Shuffle=True):
         """ studies the deck """
@@ -64,7 +71,6 @@ class Tasks:
 # this is included in the library to keep the complete functionality of the program contained
 # to one file. 
 def main():
-    Shuffle = True
     if len(sys.argv) == 1:
         print(helpstr)
     elif len(sys.argv) >= 2:
@@ -73,28 +79,20 @@ def main():
             print(helpstr)
             return
         if '-s' in sys.argv:
-            Shuffle = False
             sys.argv.pop(sys.argv.index('-s'))
+            quiz = Quiz(sys.argv)
+            quiz.study()
+        if '-S' in sys.argv:
+            quiz = Quiz(sys.argv)
+            quiz.study(Shuffle=False)
         if '-j' in sys.argv:
             sys.argv.pop(sys.argv.index('-j'))
-            quiz = Quiz(name="test_json")
-            quiz.from_file(sys.argv)
+            quiz = Quiz(sys.argv)
             quiz.to_json()
-
     else:
         print('something went wrong')
     
-    quiz = Quiz()
-
-    try: # needs to be a try, except else... for now
-        quiz.from_file(sys.argv)
-    except:
-        print(sys.argv, 'don\'t seem to exist')
-
-    if Shuffle == True:
-        quiz.study()
-    else:
-        quiz.study(Shuffle=False)
+    quiz = Quiz(sys.argv)
 
 if __name__ == '__main__':
         main()
