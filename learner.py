0601020736c7633ffc4938fc2935e90781cd55a7
Eric Nutter <nttereg@gmail.com>
2020-05-27 20:50:46 -0500

    implemented save and cleaned codebase

--- learner.py
+++ learner.py
@@ -1,5 +1,6 @@
 import sys
 import json
+from os import path
 from random import shuffle
 
 """
@@ -21,48 +22,62 @@ Flags:
 """
 
 class Quiz:
+
     def __init__(self, filelist = []):
         self.name = ''
         self.cards = []
-        self.size = 0
         self.json = {}
 
         if filelist != []:
-            
-            # simple for now
-            self.name = filelist[1]
+            self.name = filelist[0].split('.')[0]
 
-            decks = []
             for file in filelist:
-                names = filelist
-                names = [ names.pop(names.index(filename)) for filename in names ]
-                #print('names = ', names)
                 try:
                     with open(file, 'r') as deckfile:
-                        decks.append(deckfile.readlines())
+                        [ self.cards.append(card) for card in deckfile.readlines() ]
+                except OSError:
+                    raise OSError
                 except IOError:
-                    print(file, "does not appear to exist")
-                    break
+                    raise IOError
 
-            [[ self.cards.append(card) for card in deck ]  for deck in decks ]
+    def __len__(self):
+        l = len(self.cards)
+        l += len(self.json)
+        return l
 
-            self.size = len(self.cards)
-
-
-
-    def to_string(self):
-        """converts a json Quiz to the "legacy" format"""
-        pass
-
+    def to_legacy(self):
+        #pass
+        if len(self) == 0:
+            print("need to figure out what kind of error this is")
+        else:
+            self.cards = self.json['cards']
+            
     def to_json(self):
-        """converts a String format deck to json"""
-        self.json = json.dumps({"name": self.name, "qa": [self.cards]}, \
-                                separators=(':', ','), \
-                                sort_keys=True, \
-                                indent=4)
+        if len(self.json) == 0:
+            self.json = json.dumps({"name": self.name, "qa": [self.cards]}, \
+                                    separators=(':', ','), \
+                                    sort_keys=True, \
+                                    indent=4)
+    
+    def save(self):
+        """ saves the deck in both classical or json formats """
+        legacy_name = self.name + '.txt'
+        json_name = self.name + '.json'
+        if not path.isfile(legacy_name):
+            if self.cards == []:
+                self.to_legacy()
+            with open(legacy_name, 'w') as f:
+                for c in self.cards:
+                    f.write(c)
+        
+        if not path.isfile(json_name):
+            if self.json == {}:
+                self.to_json()
+            with open(json_name, 'w') as f:
+                f.write(self.json)
     
     def study(self, Shuffle=True):
-        """ studies the deck """
+        """ study the deck at the command line """
         if Shuffle == True:
             shuffle(self.cards)
 
@@ -76,6 +91,8 @@ class Quiz:
 class Notes:
     """This may never get implemented"""
     pass
+    #__init__(self, notes_dir):
+    #    self.dir = notes_dir
 
 class Tasks:
     """This may also never get implemented""" 
@@ -95,6 +112,7 @@ def main():
             sys.argv.pop(sys.argv.index('-s'))
             quiz = Quiz(sys.argv)
             quiz.study()
+            quiz.save()
         if '-S' in sys.argv:
             quiz = Quiz(sys.argv)
             quiz.study(Shuffle=False)
@@ -104,8 +122,6 @@ def main():
             quiz.to_json()
     else:
         print('something went wrong')
-    
-    quiz = Quiz(sys.argv)
 
 if __name__ == '__main__':
         main()
